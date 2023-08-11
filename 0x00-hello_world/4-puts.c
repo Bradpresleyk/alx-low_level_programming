06a89c5d6a9c9112692904c4a9b690a9352b02ce
bradpresleyk <bradpresley30@gmail.com>
2023-08-11 09:31:52 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* Printing out a string */
+/**
+ * Printing out a string
+ */ 
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
