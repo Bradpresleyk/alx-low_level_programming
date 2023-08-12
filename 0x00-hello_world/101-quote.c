e2db6730abad1f918783b34390ea24acb83b2173
bradpresleyk <bradpresley30@gmail.com>
2023-08-12 14:54:22 -0700

    fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(void)
+int main()
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 return (1);
 }
