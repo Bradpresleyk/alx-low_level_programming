b11516b716aef11c226b5d06f7d05a07ad163a73
bradpresleyk <bradpresley30@gmail.com>
2023-08-12 15:06:26 -0700

    fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
-/**
- *  main - function that outputs a string
- *  Return - 1 (Error)
- */
 #include <stdio.h>
+/** 
+ * main - function thar outputs a string
+ * Return - 1 (Error)
+ */
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
