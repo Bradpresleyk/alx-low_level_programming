1095196e8f5f46a8e6c2db362a0c25eeb380b6df
bradpresleyk <bradpresley30@gmail.com>
2023-08-12 15:02:35 -0700

    fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 /**
  *  main - function that outputs a string
+ *  Return - 1 (Error)
  */
 #include <stdio.h>
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
