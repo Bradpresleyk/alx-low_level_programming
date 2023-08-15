90bc874ffe94974ef372f2eaedf7a6bc387c2b1f
bradpresleyk <bradpresley30@gmail.com>
2023-08-15 09:49:34 -0700

    control flow

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <time.h>
 int main(void)
 {
-	int n
+	int n;
 
 	srand(time(o));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND_MAX / 2;
 	if(n > 0){
 		printf("%d is positive\n", n);
 	}
@@ -14,4 +14,5 @@ int main(void)
 	} else {
 	      printf("%d is negative\n",n);
 	      }
+	return (o);
 }
