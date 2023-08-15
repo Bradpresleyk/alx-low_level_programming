d61ba7819dcf3cdd71734bdfc60f44ca98166b78
bradpresleyk <bradpresley30@gmail.com>
2023-08-15 14:32:10 -0700

    base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Function that outputs single digits 0-9
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf('\n');
+	return (0);
+}
