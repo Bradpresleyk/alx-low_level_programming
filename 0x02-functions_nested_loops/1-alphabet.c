1cc25a0150a1009057665d8884c230bed16e9bae
bradpresleyk <bradpresley30@gmail.com>
2023-08-17 09:00:10 -0700

    abc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print - Function that prints the alphabet in lowercase
+ * print_alphabet - Function that prints the alphabet in lowercase
  * Return: 0 (Success)
  */
 void print_alphabet(void)
 {
 	int n = 97;
 	while(n <= 122)
+
 	{
 		_putchar(n);
-		n++;
-        }
+		n++;	
+	}	
 	_putchar('\n');
 }
