676b697b173ffd85c69b119c15ffb59a221f980e
bradpresleyk <bradpresley30@gmail.com>
2023-08-17 14:12:22 -0700

    letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,16 @@
  */
 int _isalpha(int c)
 {
-	 if (c > 64 && c < 91 || c > 96 && c < 123)
+	 if (c > 64 && c < 91)
 	 {
                 return (1);
 	 }
-        else
+        else if (c > 96 && c < 123)
         {
-                return (0);
+                return (1);
         }
+	 else
+	 {
+		 return (0);
+	 }
 }
