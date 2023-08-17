dde22edce00a5aa446461e38cb123b1b0b4319f6
bradpresleyk <bradpresley30@gmail.com>
2023-08-17 06:39:24 -0700

    abc

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,14 @@
 #include <unistd.h>
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
 void print_alphabet(void)
 {
                 int n = 97;
                 while(n <= 122)
         {
-                _putchar(n);
+              _putchar(n);
                 n++;
         }
         _putchar('\n');
