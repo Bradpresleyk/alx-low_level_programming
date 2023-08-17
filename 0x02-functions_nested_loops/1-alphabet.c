9ab2312da8c80a2450fb7466e3169c4ad75f1647
bradpresleyk <bradpresley30@gmail.com>
2023-08-17 04:31:23 -0700

    abc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,15 @@
  */
 int main(void)
 {
-	int n = 97;
-	while(n <= 122)
+	void print_alphabet(void)
+	{
+		int n = 97;
+		while(n <= 122)
 	{
 		putchar(n + 1);
 		n++;
 	}
 	putchar('\n');
 	return (0);
+	}
 }
