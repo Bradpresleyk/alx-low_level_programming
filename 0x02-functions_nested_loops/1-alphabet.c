e44398462b2110415c611ae0cd80da1e577ef390
bradpresleyk <bradpresley30@gmail.com>
2023-08-17 04:00:27 -0700

    lowercaseABC

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Function that prints the alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 96;
+	while(n != 123)
+	{
+		putchar(n + 1);
+		n++;
+	}
+	putchar("\n");
+	return (0);
+}
