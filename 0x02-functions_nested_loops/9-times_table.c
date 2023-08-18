fa3ca54e05b51991d5a0c2b54e9c7488b18f70f7
bradpresleyk <bradpresley30@gmail.com>
2023-08-18 16:03:52 -0700

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,13 @@ int times_table(void)
 {
 	int n = 9;
 	int i, j;
+
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			int result = i * j;
-	
+
 			if (j != 0)
 			{
 				_putchar(',');
