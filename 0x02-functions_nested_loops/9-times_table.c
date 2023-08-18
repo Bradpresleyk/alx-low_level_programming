535d5dbd8f4fdf69230a960f3afb2f1ddc825b40
bradpresleyk <bradpresley30@gmail.com>
2023-08-18 15:10:17 -0700

    9times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * times_table - Function prints nine times table
+ * Return: 0 (Success)
+ */
+int times_table(void)
+{
+	int n = 9;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= n; j++)
+		{
+			int result = i * j;
+
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(result + '0');
+			}
+			_putchar('\n');
+	}
+	return (0);
+}
