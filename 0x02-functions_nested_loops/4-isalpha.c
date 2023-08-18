9b0e332fd4b4885f3e4d82970a2c26d306c3d508
bradpresleyk <bradpresley30@gmail.com>
2023-08-18 00:38:22 -0700

    letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,20 +3,20 @@
 /**
  * _isalpha - Checks if a character is a letter
  * Return: returns 1 if character is a letter otherwise returns 0
- * c: takes an integer as a character
+ * @c: takes an integer as a character
  */
 int _isalpha(int c)
 {
-	 if (c > 64 && c < 91)
-	 {
-		 return (1);
-	 }
-	 else if (c > 96 && c < 123)
-	 {
-		 return (1);
-	 }
-	 else
-	 {
-		 return (0);
-	 }
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
