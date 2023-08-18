5acd1dcdc3c1587d4d1de37771a22dcb2c567ecb
bradpresleyk <bradpresley30@gmail.com>
2023-08-18 04:37:19 -0700

    absolutes

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,18 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * int _abs - Function that computes the absolute value of an integer
+ * _abs - Function that computes the absolute value of an integer
+ * @n: Takes an integer and returns the absolute value
  * Return: 0 (Success)
  */
 int _abs(int n)
 {
-	if(n < 0)
+	if (n < 0)
 	{
-		return -n;
+		return (-n);
 	}
 	else
 	{
-		return n;
+		return (n);
 	}
 }
