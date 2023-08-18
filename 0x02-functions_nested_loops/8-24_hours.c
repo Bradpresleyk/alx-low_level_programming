1de6e9086c2da92f219e48caed07fa52c9f62f75
bradpresleyk <bradpresley30@gmail.com>
2023-08-18 13:39:04 -0700

    24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * jack_bauer - Function outputs every minute of the day of Jack Bauer
+ * Return: 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+		}
+	}
+}
