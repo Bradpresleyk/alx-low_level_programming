b4f543d052b5b5ab42ecb9fd27ae996c96a4a7c9
bradpresleyk <bradpresley30@gmail.com>
2023-08-18 06:31:40 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,20 +3,21 @@
 /**
  * print_last_digit - Function that prints the last digit of a number
  * @n: Takes a number as an arguement
- * Return: 0 (Success)
+ * Return: Returns last digit
  */
 int print_last_digit(int n)
 {
+	int m;
 	if (n < 0)
 	{
 		n = -n;
-		int m = n % 10;
+		m = n % 10;
 		_putchar(m + '0');
 		return (m);
 	}
 	else
 	{
-		int m = n % 10;
+		m = n % 10;
 		_putchar(m + '0');
 		return (m);
 	}
