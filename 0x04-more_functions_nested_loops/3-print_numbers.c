fa8a4816ce09b3e106cbb83ae190d041c45cba17
bradpresleyk <bradpresley30@gmail.com>
2023-08-20 16:43:47 -0700

    0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_numbers - Function that prints numbers from 0-9
+ * Return: 0 (Success)
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while(n <= 9)
+	{
+	_putchar(n + '0');
+	_putchar('\n');
+	n++;
+	}
+}
