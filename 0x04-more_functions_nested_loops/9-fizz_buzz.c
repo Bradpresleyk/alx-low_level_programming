fb9444fcaa76bb69a6f59e9e7f6f962928f1c4ae
bradpresleyk <bradpresley30@gmail.com>
2023-08-21 05:45:54 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-	if (i % 3 == 0)
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz\n");
+	}
+	else if (i % 3 == 0)
 	{
 	printf("Fizz\n");
 	}
 	else if (i % 5 == 0)
 	{
 	printf("Buzz\n");
-	}
-	else if (i % 3 == 0 && i % 5 == 0)
-	{
-	printf("FizzBuzz\n");
 	}
 	else
 	{
