7138925854b5f457ec78806e8e158dae895cab5d
bradpresleyk <bradpresley30@gmail.com>
2023-08-21 05:29:54 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Function that prints numbers 0-100 except multiples of 3,5 3 and 5
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+	if (i % 3 == 0)
+	{
+	printf("Fizz");
+	printf('\n');
+	}
+	else if (i % 5 == 0)
+	{
+	printf("Buzz");
+	printf('\n')
+	}
+	else if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz");
+	printf('\n')
+	}
+	else
+	{
+	printf(i);
+	printf('\n');
+	}
+}
