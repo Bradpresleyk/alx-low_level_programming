a9f486979a8ded9eba0c8cc5ad97866d72b1a589
bradpresleyk <bradpresley30@gmail.com>
2023-08-20 17:06:11 -0700

    continue

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_most_numbers - Function that prints numbers from 0-9 except 2 and 4
+ * Return: 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+	if (n == 2 || n == 4)
+	{
+	continue;
+	}
+	_putchar(n + '0');
+	n++;
+	}
+	_putchar('\n');
+}
