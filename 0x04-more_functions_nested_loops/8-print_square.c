74cd16359ccba5ec2037f634fa3e7444fdf625b4
bradpresleyk <bradpresley30@gmail.com>
2023-08-21 04:25:08 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_square - Function that prints a square according to size
+ * @size: Parameter that takes a size value
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+}
