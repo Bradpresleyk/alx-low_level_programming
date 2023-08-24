811d73e4b72330644a0e90b0347ede5d63f8d77c
bradpresleyk <bradpresley30@gmail.com>
2023-08-24 14:13:18 -0700

    Dereferencing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * reset_to_98 - Function that updates the pointer value to 98
- * @*n: a pointer with the type int
+ * @n: a pointer with the type int
  */
 void reset_to_98(int *n)
 {
