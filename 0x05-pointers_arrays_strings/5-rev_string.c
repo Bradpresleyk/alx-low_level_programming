b90c61822b1ce10dd7f590216064fb86f5a32cdc
bradpresleyk <bradpresley30@gmail.com>
2023-08-25 13:11:49 -0700

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,18 +6,9 @@
  */
 void rev_string(char *s)
 {
-	int length = 0;
-
-	while (*s != '\0')
-	{
-	length++;
-	s++;
-	}
-	s--;
-	while (length > 0)
+	while (*s > 0)
 	{
 	_putchar(*s);
 	s--;
-	length--;
 	}
 }
