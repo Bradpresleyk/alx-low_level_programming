14c4bf4d21ca7a36c133e750656ff312c34ec4ce
bradpresleyk <bradpresley30@gmail.com>
2023-08-25 11:21:05 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_rev - Function that prints a string in reverse followed by newline
+ * @s: String to reverse
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+	s++;
+	length++;
+	}
+	while (length > 0)
+	{
+	_putchar(*s);
+	s--;
+	length--;
+	}
+	_putchar('\n');
+}
