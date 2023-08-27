7ba87ae72856915604a90a8855c00caf2d1ba2c6
bradpresleyk <bradpresley30@gmail.com>
2023-08-27 14:38:46 -0700

    n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_times_table - Function that prints the times table up to 15
+ * @n: tables table integer to be printed
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	if (n > 15 || n < 0)
+	{
+	_putchar("");
+	}
+	else
+	{
+	while (i < n)
+	{
+	while (j < n)
+	{
+	_putchar(i * j);
+	j++;
+	}
+	i++;
+	}
+	}
+}
