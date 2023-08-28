a8a60bb40cb1349ba34bb5f35963267d3f281011
bradpresleyk <bradpresley30@gmail.com>
2023-08-28 13:49:47 -0700

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * *string_toupper - Function that changes all string letters to uppercase
+ * @n: Pointer
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+	if (n[i] >= 'a' && n[i] <= 'z')
+	{
+	n[i] - n[i] = 32;
+	}
+	i++;
+	}
+	return (n);
+}
