2e13e7eed2fc1bff4b1a6ffeac11ba24ea5b6630
bradpresleyk <bradpresley30@gmail.com>
2023-09-03 00:46:03 -0700

    locate string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <unistd.h>
-
 /**
  * _strchr - Function that locates a character in a string
  * @s: A pointer to the string to search
@@ -12,7 +11,7 @@ char *_strchr(char *s, char c)
 
 	int i = 0;
 
-	for (; s[i] != '\0'; i++)
+	for (; s[i] >= '\0'; i++)
 	{
 	if (s[i] == c)
 	{
