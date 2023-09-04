464af0a94f368d706e0e137d52eabf8015300caa
bradpresleyk <bradpresley30@gmail.com>
2023-09-04 01:14:45 -0700

    times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,6 @@ void print_times_table(int n)
 	{
 	for (j = 1; j <= n; j++)
 	{
-
 	int result = i * j;
 
 	if (result < 10)
@@ -24,13 +23,22 @@ void print_times_table(int n)
 	_putchar(result + '0');
 	_putchar(',');
 	_putchar(' ');
+	_putchar(' ');
 	}
-	else
+	else if (result < 100)
 	{
 	_putchar((result / 10) + '0');
 	_putchar((result % 10) + '0');
+	_putchar(',');
 	_putchar(' ');
 	}
+	else
+	{
+	_putchar((result / 100) + '0');
+	_putchar((result / 10) % 10 + '0');
+	_putchar((result % 10) + '0');
+	_putchar(',');
+	_putchar(' ');
 	}
 	_putchar('\n');
 	}
