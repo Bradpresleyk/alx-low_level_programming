b0bbbffaa15b5a418b743e440c7bc1a2672dd8d4
bradpresleyk <bradpresley30@gmail.com>
2023-09-04 00:47:02 -0700

    times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,9 +12,9 @@ void print_times_table(int n)
 	{
 	return;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-	for (j = 0; j < n; j++)
+	for (j = 1; j <= n; j++)
 	{
 
 	int result = i * j;
