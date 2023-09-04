56f7d94e364b78799ddc6b1f8c374a8d25116703
bradpresleyk <bradpresley30@gmail.com>
2023-09-04 08:17:19 -0700

    String length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
 
 	if (*s == '\0')
 	{
-	return (length);
+	return (0);
 	}
-	_strlen_recursion(s + 1);
-	length++;
+	return (1 + _strlen_recursion(s + 1));
 }
