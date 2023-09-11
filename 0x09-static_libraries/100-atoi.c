5d92ae16e4ade28f7451b761f8b0bb9f14a26e40
bradpresleyk <bradpresley30@gmail.com>
2023-09-11 09:57:24 -0700

    static

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <unistd.h>
+/**
+ * _atoi - Function description
+ * @s: input pointer
+ * Return: 0 (Success)
+ */
 int _atoi(char *s)
 {
 
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _putchar: Function description
+ * _putchar - Function description
+ * @c: input
+ * Return: 0 (Success)
  */
 int _putchar(char c)
 {
