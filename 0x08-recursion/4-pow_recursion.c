9bac337943dc776df14b22af11e043a5ac3f6130
bradpresleyk <bradpresley30@gmail.com>
2023-09-27 08:49:06 -0700

    X raised to the power of Y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,10 +14,10 @@ int _pow_recursion(int x, int y)
 	{
 	return (-1);
 	}
-	if (y == 1)
+	if (y == 0)
 	{
-	return (x * y);
+	return (x * sum);
 	}
-	sum =  _pow_recursion(x, y - 1);
+	sum *=  _pow_recursion(x * x, y - 1);
 	return (sum);
 }
