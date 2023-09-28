b83b4234b89d0dcfc646f1ca24df45715c8585b7
bradpresleyk <bradpresley30@gmail.com>
2023-09-28 02:32:52 -0700

    Sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,15 +5,24 @@
  * @n: Number to find square root of
  * Return: Returns the square root of a number otherwise returns -1
  */
-int _sqrt_recursion(int n)
+int int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	int original = n;
+	int test;
+
+	if (n <= 0)
 	{
 	return (-1);
 	}
-	if (n <= 1)
+	if (n < 1)
 	{
 	return (n);
 	}
-	return (_sqrt_recursion(n - 1));
+	test = sqrt_recursion(n - 1);
+
+	if (test * test > n)
+	{
+	return (test - 1);
+	}
+	return (test);
 }
