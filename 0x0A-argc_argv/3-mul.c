3efc3ea17c7a5eb5781ab42867709e133259d85a
bradpresleyk <bradpresley30@gmail.com>
2023-10-02 10:00:13 -0700

    Multply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Function that multiplies two numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 if success and 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 1;
+	int i;
+
+	if (argc - 1 == 2)
+	{
+	for (i = 1; i < argc; i++)
+	{
+	sum *= atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+	}
+	else if (argc - 1 != 2)
+	{
+	printf("%s\n", "Error");
+	return (1);
+	}
+}
