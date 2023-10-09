9456329e439f56651a7c47c15a92112e65c7c910
bradpresleyk <bradpresley30@gmail.com>
2023-10-09 05:57:48 -0700

    Static

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,2 @@
-C Static libraries
+#!/bin/bash
+Static Libraries
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
+void _puts(char *str);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
