0844054cdf791fb8d55dd2f176a5266a16fb5f68
bradpresleyk <bradpresley30@gmail.com>
2023-10-17 08:41:09 -0700

    Reallocating memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdlib.h>
 /**
 * _realloc - reallocates a memory block using malloc and free
@@ -10,38 +9,39 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p;
-	unsigned int i, n = new_size;
-	char *oldp = ptr;
+	char *nptr;
+	unsigned int i;
 
-	if (ptr == NULL)
-	{
-	p = malloc(new_size);
-	return (p);
-	}
-	else if (new_size == 0)
-	{
-	free(ptr);
-	return (NULL);
-	}
-	else if (new_size == old_size)
+	if (new_size == old_size)
 	{
 	return (ptr);
-	p = malloc(new_size);
 	}
-	if (p == NULL)
+	if ((new_size == 0) && (ptr != NULL))
+	{
+	free(ptr);
+	return (NULL);
+	}
+	if (ptr == NULL)
+	{
+	nptr = malloc(new_size);
+	if (nptr == NULL)
 	{
 	return (NULL);
 	}
-	if (new_size > old_size)
-	{
-	n = old_size;
 	}
-	for (i = 0; i < n; i++)
+	if (new_size > old_size && (ptr != NULL))
 	{
-	p[i] = oldp[i];
+	ntpr = malloc(new_size);
+	if (ntpr == NULL)
+	{
+	return (ntpr);
+	}
+	for (i = 0; i < old_size; i++)
+	{
+	ntpr[i] = *((char *)ptr + 1);
 	}
 	free(ptr);
-	return (p);
-
+	}
+	}
+	return (nptr);
 }
