1447db58902ad916debf830993a539a23bc6f925
bradpresleyk <bradpresley30@gmail.com>
2023-11-16 02:14:49 -0800

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,35 @@
-#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
- * create_file - function that creates a file
+ * create_file - Function that creates a file
  * @filename: filename
- * @text_content: content writed in the file
- * Return: 1 if it success or -1 if it fails
+ * @text_content: content written in the file
+ * Return: 1 on success and -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int file_d;
-	int nletters;
-	int rwr;
+	ssize_t lenw;
 
-	if (!filename)
+	if (filename == NULL)
 	{
 	return (-1);
 	}
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_d == -1)
 	{
 	return (-1);
 	}
-	if (!text_content)
+	if (text_content != NULL)
 	{
-	text_content = "";
-	}
-	for (nletters = 0; text_content(nletters); nletters++)
-		;
-	rwr = write(file_d, text_content, nletters);
-
-	if (rwr == -1)
+	lenw = write(file_d, text_content, strlen(text_content));
+	if (lenw == -1)
 	{
+	close(file_d);
 	return (-1);
 	}
+	}
 	close(file_d);
-
 	return (1);
 }
