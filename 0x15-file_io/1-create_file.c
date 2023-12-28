e7b093b91ce961fa13bc37745e276215e6e491a0
bradpresleyk <bradpresley30@gmail.com>
2023-12-28 11:43:06 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int *buffer;
 	ssize_t w;
 
-	fp = open(filename, O_WRONLY | O_CREAT | O_EXCL);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fp == -1)
 	{
 	return (-1);
