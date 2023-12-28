eb3cb31b9e1ac6822eaa8dab7b59c72442aa83f4
bradpresleyk <bradpresley30@gmail.com>
2023-12-28 11:40:22 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,18 +13,10 @@ int create_file(const char *filename, char *text_content)
 	ssize_t fp;
 	int *buffer;
 	ssize_t w;
-	mode_t permissions = S_IRUSR | S_IWUSR;
 
-	fp = open(filename, O_WRONLY | O_CREAT | O_EXCL, permissions);
+	fp = open(filename, O_WRONLY | O_CREAT | O_EXCL);
 	if (fp == -1)
 	{
-	return (-1);
-	}
-	if (fchmod(fp, permissions) == -1)
-	{
-	perror("Error setting file permissions");
-	close(fp);
-	unlink(filename);
 	return (-1);
 	}
 	buffer = malloc(sizeof(char) * sizeof(text_content));
