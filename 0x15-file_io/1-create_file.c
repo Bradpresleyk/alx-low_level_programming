3f1900bbe526590c41d9716af8752de28210cc68
bradpresleyk <bradpresley30@gmail.com>
2023-12-28 11:35:03 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	perror("Error setting file permissions");
 	close(fp);
 	unlink(filename);
-	return (1);
+	return (-1);
 	}
 	buffer = malloc(sizeof(char) * sizeof(text_content));
 	w = write(fp, buffer, *text_content);
