d75d6dad501883f211dc6c89fe2c626b8ce83443
bradpresleyk <bradpresley30@gmail.com>
2023-12-28 11:33:31 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include "main.c"
 /**
  * create_file - Creates a file
  * @filename: name of file
@@ -12,11 +13,19 @@ int create_file(const char *filename, char *text_content)
 	ssize_t fp;
 	int *buffer;
 	ssize_t w;
+	mode_t permissions = S_IRUSR | S_IWUSR;
 
-	fp = open(filename, O_CREAT);
+	fp = open(filename, O_WRONLY | O_CREAT | O_EXCL);
 	if (fp == -1)
 	{
 	return (-1);
+	}
+	if (fchmod(fp, permissions) == -1)
+	{
+	perror("Error setting file permissions");
+	close(fp);
+	unlink(filename);
+	return (1);
 	}
 	buffer = malloc(sizeof(char) * sizeof(text_content));
 	w = write(fp, buffer, *text_content);
