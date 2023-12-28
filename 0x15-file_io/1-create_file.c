67143da15de0ebe5d399afff518e8a1777c70fdb
bradpresleyk <bradpresley30@gmail.com>
2023-12-28 11:20:01 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,8 +9,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize fp;
-	char *buffer;
+	ssize_t fp;
+	int *buffer;
 	ssize_t w;
 
 	fp = open(filename, O_CREAT);
@@ -18,8 +18,8 @@ int create_file(const char *filename, char *text_content)
 	{
 	return (-1);
 	}
-	buffer = malloc(sizeof(char) * text_content);
-	w = write(fp, buffer, text_content);
+	buffer = malloc(sizeof(char) * sizeof(text_content));
+	w = write(fp, buffer, *text_content);
 	free(buffer);
 	close(fp);
 	return (1);
