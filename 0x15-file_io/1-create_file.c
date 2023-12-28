52e92ff5d6f01d53277e988b2e545a02dc4b198f
bradpresleyk <bradpresley30@gmail.com>
2023-12-28 11:04:36 -0800

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_file - Creates a file
+ * @filename: name of file
+ * @text_content: String to write to file
+ * Return: 1 on success and 0 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+	char *buffer;
+	ssize_t w;
+
+	fp = open(filename, O_CREAT);
+	if (fp == -1)
+	{
+	return (-1);
+	}
+	buffer = malloc(sizeof(char) * text_content);
+	w = write(fp, buffer, text_content);
+	free(buffer);
+	close(fp);
+	return (1);
+}
